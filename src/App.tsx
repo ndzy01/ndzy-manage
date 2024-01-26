6699fe1aa6a88584013f848f360f8931df0918cb
张一 <zy01066705@alibaba-inc.com>
2024-01-26 10:29:07 +0800

    chore:

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import type { TabsProps } from 'antd';
 import Login from './Login';
 import Register from './Register';
 
+const links = [
+  { src: 'https://ndzy01.gitee.io/ndzy-admin', name: 'admin' },
+  { src: 'https://ndzy01.gitee.io/ndzy-todos/', name: 'todos' },
+];
+
 const items: TabsProps['items'] = [
   {
     key: '1',
@@ -17,7 +22,20 @@ const items: TabsProps['items'] = [
 ];
 
 function App() {
-  return <Tabs defaultActiveKey="1" items={items} />;
+  return (
+    <div style={{ padding: 16 }}>
+      <ul>
+        {links.map((item) => (
+          <li key={item.src}>
+            <a target="_blank" href={item.src}>
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <Tabs defaultActiveKey="1" items={items} />;
+    </div>
+  );
 }
 
 export default App;
